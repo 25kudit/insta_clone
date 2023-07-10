8a4db1c81674cb2d0234614a9280e19da517338d
udit kansal <uditkansal2002@gmail.com>
2023-07-10 22:45:14 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone/responsive/mobile_screen_layout.dart';
 import 'package:instaclone/responsive/responsive_layout_screen.dart';
 import 'package:instaclone/responsive/web_screen_layout.dart';
 import 'package:instaclone/utils/colors.dart';
+import 'package:instaclone/utils/firebase_config.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: firebaseConfig,
+    );
+  }
+  else{
+    await Firebase.initializeApp();
+  }
+  
   runApp(const MyApp());
 }
 
@@ -19,7 +32,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(webScreenLayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout()),
+      home: const ResponsiveLayout(
+          webScreenLayout: WebScreenLayout(),
+          mobileScreenLayout: MobileScreenLayout()),
     );
   }
 }
---
+++ lib/utils/firebase_config.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+const firebaseConfig = FirebaseOptions(
+  apiKey: "AIzaSyCDyAyzlj-Gh6-az7uEZvd4-pw21ADzGFk",
+  authDomain: "instaclone-cdf00.firebaseapp.com",
+  projectId: "instaclone-cdf00",
+  storageBucket: "instaclone-cdf00.appspot.com",
+  messagingSenderId: "689248380769",
+  appId: "1:689248380769:web:e4b75bf4488837269e4720",
+);
